650f9ac412f6128d20b2f5141a4f4d9cf0caff24
Juanjo7z7 <116236239+Juanjo7z7@users.noreply.github.com>
2023-05-16 23:36:34 -0500

    Cambies en el main.py en el login

--- app/main.py
+++ app/main.py
@@ -31,7 +31,6 @@ def Perifericos():
 def Perifericosimg(): 
     return render_template('public/Perifericos.html')
 msg=""
-check_password_hash=False
 
 @app.route('/registroUsuario', methods=['GET', 'POST'])
 def registroUsuario():
@@ -41,7 +40,7 @@ def registroUsuario():
         Apellido             = request.form['Apellido']
         Telefono              = request.form['Telefono']
         Email         = request.form['Email']
-        Contrasena         = generate_password_hash  (request.form['Contrasena' ],method='sha256')
+        Contrasena         = request.form['Contrasena' ]
         conexion_MySQLdb = connectionBD()
         cursor           = conexion_MySQLdb.cursor(dictionary=True)
         
@@ -81,7 +80,6 @@ def registrarForm():
         cursor.execute('INSERT INTO form_contacto (Nombre, Email, Mensaje) VALUES (%s, %s, %s)', (Nombre, Email, Mensaje))
         ResultInsert = conexion_MySQLdb.commit()
         '''
-            
         sql         = ("INSERT INTO form_contacto (Nombre, Email, Mensaje) VALUES (%s, %s, %s)")
         valores     = (Nombre, Email, Mensaje)
         cursor.execute(sql, valores)
@@ -90,53 +88,34 @@ def registrarForm():
         cursor.close() #Cerrando conexion SQL
         conexion_MySQLdb.close() #cerrando conexion de la BD
         msg = 'Registro con exito'
-
         print(cursor.rowcount, "registro insertado")
         print("1 registro insertado, id", cursor.lastrowid)
         return render_template('public/pagina1.html', msg='Formulario enviado')
     else:
         return render_template('public/pagina1.html', msg = 'Metodo HTTP incorrecto')
-    
-def dataLoginSesion():
-    inforLogin = {
-        "idLogin"             :session['id'],
-        "tipoLogin"           :session['tipo_user'],
-        "nombre"              :session['nombre'],
-        "apellido"            :session['apellido'],
-        "emailLogin"          :session['email'],
-        "sexo"                :session['sexo'],
-        "pais"                      :session['pais'],
-        "create_at"                 :session['create_at'],
-        "te_gusta_la_programacion"  :session['te_gusta_la_programacion']
-    }
-    return inforLogin
 
 @app.route('/loginUsuario', methods=['POST'])
 def loginUser():
     conexion_MySQLdb = connectionBD()
     if 'conectado' in session:
-        return render_template('public/pagina1.html', dataLogin = dataLoginSesion())
+        return render_template('public/pagina1.html')
     else:
         msg = ''
-        if request.method == 'POST' and 'Email' in request.form and 'Contrasena' in request.form:
+    if request.method == 'POST' and 'Email' in request.form and 'Contrasena' in request.form:
             Email        = str(request.form['Email'])
             Contrasena   = str(request.form['Contrasena'])
             
             # Comprobando si existe una cuenta
             cursor = conexion_MySQLdb.cursor(dictionary=True)
-            cursor.execute("SELECT * FROM registro_usuario WHERE Email = %s ",  [Email])
+            cursor.execute("SELECT * FROM registro_usuario WHERE Email = %s AND Contrasena =%s",  [Email, Contrasena,])
             account = cursor.fetchone()
             if account:
-                    if check_password_hash(account['Contrasena'],Contrasena):
-                        msg = "Ha iniciado sesión correctamente."
-                        return render_template('public/pagina1.html', msjAlert = msg, typeAlert=1)                    
-                    else:
-                        msg = 'Datos incorrectos, por favor verfique!'
-                        return render_template('public/Registro.html', msjAlert = msg, typeAlert=0)
-                    
-            else: 
-                msg="Usuario no encontrado "
-                
+                    # Crear datos de sesión, para poder acceder a estos datos en otras rutas 
+                    msg = "Ha iniciado sesión correctamente."
+                    return render_template('public/pagina1.html', msjAlert = msg, typeAlert=1)                    
+            else:
+                    msg = 'Datos incorrectos, por favor verfique!'
+                    return render_template('public/Registro.html', msjAlert = msg, typeAlert=0)
 if __name__ == '__main__': 
     app.run(debug=True, port=666) 
 
